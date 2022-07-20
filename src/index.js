0d99362b8ce5530781cb66c483205cbe9956ab75
Viktor <viktor.shchokin.a@gmail.com>
2022-07-20 17:35:19 +0300

    added basic react routes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BorwserRouter} from 'react-router-dom';
 
 import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(
-    <App/>
-)
+    <BorwserRouter>
+        <App/>
+    </BorwserRouter>
+);
