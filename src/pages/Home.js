168ad6db23d1ba827de3562d9e7ce60be69e4c11
Viktor <viktor.shchokin.a@gmail.com>
2022-07-22 14:53:15 +0300

    added exercises comp

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Box } from '@mui/material';
 
 import Exercises from '../components/Exercises';
@@ -6,11 +6,21 @@ import HeroBanner from '../components/HeroBanner';
 import SearchExercises from '../components/SearchExercises';
 
 const Home = () => {
+  const [bodyPart, setBodyPart] = useState('all');
+  const [exercises, setExercises] = useState([]);
   return (
     <Box>
       <HeroBanner/>
-      <SearchExercises/>
-      <Exercises/>
+      <SearchExercises 
+        setExercises={setExercises} 
+        bodyPart = {bodyPart} 
+        setBodyPart = {setBodyPart}
+      />
+      <Exercises
+        exercises = {exercises}
+        setExercises={setExercises} 
+        bodyPart = {bodyPart} 
+      />
     </Box>
   )
 }
